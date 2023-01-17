eabc4c313b99044795fea0dc2edb365dfb908948
Carter <carterandrewrose@gmail.com>
2023-01-17 03:10:42 -0700

    adding revisions

--- anim/src/App.js
+++ anim/src/App.js
@@ -1,68 +1,66 @@
 import './App.css';
 import React,{useEffect, useState} from 'react';
 function App() {
-  const [percent,setPercent]=useState()
-  const [mouse,setMouse]=useState()
+  const [last,setLast]=useState()
+  const [percent,setPercent]=useState(0)
+  const [mouse,setMouse]=useState(0)
   const [x, setX] = useState()
   const [windo,setWindow]=useState([window.innerHeight,window.innerWidth])
+  
+  const handleOnMove = e => {
+    
+    if(mouse == 0) return;
+    //sees if it hasnt been dragged yet
+    var mouseDelta = (x) - mouse;
+    var  maxDelta = window.innerWidth / 2;
+    setPercent(parseFloat((mouseDelta / maxDelta) * -100))
+    var nextPercentageUnconstrained = parseFloat(last) + percent,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    console.log('hi!')
+    e.animate({
+      transform: `translate(${nextPercentage}%, -50%)`
+    }, { duration: 1200, fill: "forwards" });
+    
+    for(const image of e.getElementsByClassName("image")) {
+      image.animate({
+        objectPosition: `${100 + nextPercentage}% center`
+      }, { duration: 1200, fill: "forwards" });
+    }
+  }
     useEffect(
       () => {
+        const update = (e)=>{
+          setX(e.x)
+        }
         const onDown = e =>{
-          console.log(e.x)
           setX(e.x)
           setMouse(e.x)
-          console.log(x+" hello " +mouse)
         }
         const handleWindowResize = () => {
           setWindow([window.innerWidth]);
         };
         const onUp = e =>{
           setMouse(0)
+          setLast(percent)
         }
-        const onMove = e => {
-          if(mouse == 0) return;
-
-          setMouse(e.x)
-          //sees if it hasnt been dragged yet
-          const mouseDelta = (e.x) - mouse,
-                maxDelta = window.innerWidth / 2;
-          const percentage = (mouseDelta / maxDelta) * -100,
-                nextPercentageUnconstrained = parseFloat(e.dataset.prevPercentage) + percentage,
-                nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-                
-          
-          e.dataset.percentage = nextPercentage;
-          
-          e.animate({
-            transform: `translate(${nextPercentage}%, -50%)`
-          }, { duration: 1200, fill: "forwards" });
-          
-          for(const image of e.getElementsByClassName("image")) {
-            image.animate({
-              objectPosition: `${100 + nextPercentage}% center`
-            }, { duration: 1200, fill: "forwards" });
-          }
-        }
-        
+        window.addEventListener('mousemove', update)
+      window.addEventListener('touchmove', update)
         window.addEventListener('mouseup',onUp)
-        window.addEventListener('mousemove', onMove)
-        window.addEventListener('touchmove', onMove)
         window.addEventListener('mousedown',onDown)
         window.addEventListener('resize',handleWindowResize)
         return () => {
-          
-        window.addEventListener('mouseup',onUp)
-          window.removeEventListener('mousemove', onMove)
-          window.addEventListener('touchmove', onMove)
-        window.addEventListener('mousedown',onDown)
+          window.removeEventListener('mousemove', update)
+        window.removeEventListener('touchmove', update)
+        window.removeEventListener('mouseup',onUp)
+        window.removeEventListener('mousedown',onDown)
           window.removeEventListener('resize', handleWindowResize);
         }
       },
-      [setX]
+      [setX, setMouse,setWindow,setPercent]
     )
   return (
-    <body className="App">
-      <div id='track'>
+    <body className="App" onPointerMove={handleOnMove}>
+      <div id='track' >
         <img class='image' draggable='false' alt='machined parts'src='https://images.unsplash.com/photo-1567093322503-341d262ad8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1hY2hpbmVyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'></img>
         <img class='image' draggable='false' alt='machined parts' src='https://images.unsplash.com/photo-1567177173026-402dd75a5ab7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1hY2hpbmVyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'></img>
         <img class='image' draggable='false' alt='machined parts' src='https://images.unsplash.com/photo-1627807452502-0c8329fc710b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fG1hY2hpbmVyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'></img>
